0469bb5c3f80591fa413e6cd9292b0240ca828e0
rishabh-preethan <rishabh_preethan@thirdray.ai>
2024-07-02 07:08:34 +0000

    Added smooth lines to echart

--- src/components/ParentComponent.svelte
+++ src/components/ParentComponent.svelte
@@ -8,7 +8,7 @@
       {
         // Configuration for the first chart
         title: {
-          text: 'ECharts Example 1', // Title of the chart
+          text: 'Example 1', // Title of the chart
         },
         xAxis: {
           type: 'category', // X-axis is of category type
@@ -27,7 +27,7 @@
       {
         // Configuration for the second chart
         title: {
-          text: 'ECharts Example 2', // Title of the chart
+          text: 'Example 2', // Title of the chart
         },
         xAxis: {
           type: 'category', // X-axis is of category type
@@ -39,13 +39,14 @@
         series: [
           {
             type: 'line', // Series type is line
+            smooth: true,
             data: [150, 230, 224, 218, 135, 147, 260], // Data points for the series
           },
         ],
       },
       {
         title: {
-            text: 'Stacked Bar Chart Example 3', // Title of the chart
+            text: ' Example 3', // Title of the chart
         },
         tooltip: {
             trigger: 'axis',
@@ -80,7 +81,7 @@
       },
       {
         title: {
-            text: 'Area Chart Example 4', // Title of the chart
+            text: 'Example 4', // Title of the chart
         },
         tooltip: {
             trigger: 'axis',
@@ -94,7 +95,7 @@
         xAxis: {
             type: 'category', // X-axis is of category type
             boundaryGap: false, // Boundary gap is set to false to align the area chart with the X-axis
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'], // Categories on the X-axis
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Extra dp1', 'Extra dp2'], // Categories on the X-axis
         },
         yAxis: {
             type: 'value', // Y-axis is of value type
@@ -103,16 +104,18 @@
             {
             name: 'Series 1', // Name of the first series
             type: 'line', // Series type is line
+            smooth: true,
             stack: 'total', // Stack the lines
             areaStyle: {}, // This property fills the area under the line
-            data: [120, 200, 150, 80, 70, 110, 130], // Data points for the first series
+            data: [120, 200, 150, 80, 70, 110, 130, 100, 200], // Data points for the first series
             },
             {
             name: 'Series 2', // Name of the second series
             type: 'line', // Series type is line
+            smooth: true,
             stack: 'total', // Stack the lines
             areaStyle: {}, // This property fills the area under the line
-            data: [60, 80, 90, 40, 50, 70, 60], // Data points for the second series
+            data: [60, 80, 90, 40, 50, 70, 60, 50, 90], // Data points for the second series
             },
         ],
       },
